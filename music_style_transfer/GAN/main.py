7c0fdc3c27c9bb138ad208201e75d5c2cd5ebb8f
Miguel G <miguelrgraca@gmail.com>
2018-09-14 18:12:06 +0200

    added toy function to start a training run

--- music_style_transfer/GAN/main.py
+++ music_style_transfer/GAN/main.py
@@ -5,22 +5,6 @@ from . import trainer
 
 
 def create_toy_model_configs(data):
-    encoder_config = model.TransformerConfig(
-        n_layers=2,
-        model_dim=32,
-        hidden_dim=32
-    )
-
-    embedding_config = model.EmbeddingConfig(
-        input_dim=data.num_tokens(),
-        hidden_dim=32
-    )
-
-    class_embedding_config = model.EmbeddingConfig(
-        input_dim=data.num_classes(),
-        hidden_dim=32
-    )
-
     generator_config = model.GeneratorConfig(
         encoder_config=model.TransformerConfig(
             n_layers=2,
@@ -51,11 +35,37 @@ def create_toy_model_configs(data):
             hidden_dim=32),
         output_layer_config=model.OutputLayerConfig(
             output_dim=1,
-            softmax=False)
+            softmax=False),
+        class_output_layer_config=None
     )
 
     return generator_config, discriminator_config
 
-def main():
+
+def create_toy_train_config():
+    config = trainer.TrainConfig(batch_size=1,
+                                 d_optimizer=trainer.OptimizerConfig(
+                                     learning_rate=1e-4,
+                                     optimizer='adam'
+                                 ),
+                                 g_optimizer=trainer.OptimizerConfig(
+                                     learning_rate=1e-4,
+                                     optimizer='adam'
+                                 ))
+    return config
+
+
+def main_toy():
     dataset = data.ToyData()
 
+    g_config, d_config = create_toy_model_configs(dataset)
+
+    generator = model.Generator(config=g_config)
+    discriminator = model.Discriminator(config=d_config)
+
+    t = trainer.Trainer(config=create_toy_train_config(),
+                        generator=generator,
+                        discriminator=discriminator)
+
+    t.fit(dataset=dataset,
+          epochs=10)
